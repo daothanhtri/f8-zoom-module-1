7e4a5c886bc47a69935782fe749ba238a0b8dd96
Dao Thanh Tri <daothanhtrix99@gmail.com>
2025-05-19 15:27:59 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: path.resolve(__dirname, "src/index.html"),
-        index: path.resolve(__dirname, "src/about.html"),
+        about: path.resolve(__dirname, "src/about.html"),
         // Thêm trang khác nếu có
       },
     },
